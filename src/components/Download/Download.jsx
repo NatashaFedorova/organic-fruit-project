47d3055b6e5d0f96ddef11c1bec8213efddc9546
Наташа Федорова <98808643+NatashaFedorova@users.noreply.github.com>
2023-03-29 02:22:26 +0300

    fix

--- src/components/Download/Download.jsx
+++ src/components/Download/Download.jsx
@@ -1,3 +1,4 @@
+import AnimationBox from 'components/constants/AnimationBox';
 import {
   DownloadSection,
   DownloadContainer,
@@ -22,28 +23,38 @@ const Download = () => {
       <DownloadContainer>
         <Img src={ImgPhones} alt="phones" />
         <Info>
-          <PreTitle>Download app</PreTitle>
-          <Title>An easy way to order organic fruit</Title>
-          <Text>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer...
-          </Text>
+          <AnimationBox animateIn={['animate__fadeInDown']} duration={1.25}>
+            <PreTitle>Download app</PreTitle>
+          </AnimationBox>
+          <AnimationBox animateIn={['animate__fadeInRight']} duration={1.25}>
+            <Title>An easy way to order organic fruit</Title>
+          </AnimationBox>
+          <AnimationBox animateIn={['animate__fadeInRight']} duration={1.5}>
+            <Text>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer...
+            </Text>
+          </AnimationBox>
           <BoxForIcons>
-            <Link
-              href="https://www.apple.com/ua/app-store/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <ImgIcon src={IconAppStore} alt="icon" />
-            </Link>
-            <Link
-              href="https://play.google.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <ImgIcon src={IconGoolePlay} />
-            </Link>
+            <AnimationBox animateIn={['animate__flipInX']} duration={1.25}>
+              <Link
+                href="https://www.apple.com/ua/app-store/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <ImgIcon src={IconAppStore} alt="icon" />
+              </Link>
+            </AnimationBox>
+            <AnimationBox animateIn={['animate__flipInX']} duration={1.25}>
+              <Link
+                href="https://play.google.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <ImgIcon src={IconGoolePlay} />
+              </Link>
+            </AnimationBox>
           </BoxForIcons>
         </Info>
       </DownloadContainer>
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,3 +1,4 @@
+import AnimationBox from 'components/constants/AnimationBox';
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import {
   ReviewsContainer,
@@ -22,24 +23,31 @@ const Reviews = () => {
       <ReviewsContainer>
         <Box>
           <Info>
-            <Title>What our customers say about us</Title>
-            <Text>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularis
-            </Text>
-            <Name>Ahmed Hridoy</Name>
-            <div>
-              <AiFillStar size={22} color="#F1B90B" />
-              <AiFillStar size={22} color="#F1B90B" />
-              <AiOutlineStar size={22} color="#F1B90B" />
-              <AiOutlineStar size={22} color="#F1B90B" />
-              <AiOutlineStar size={22} color="#F1B90B" />
-            </div>
+            <AnimationBox animateIn={['animate__fadeInDown']} duration={1.25}>
+              <Title>What our customers say about us</Title>
+            </AnimationBox>
+            <AnimationBox animateIn={['animate__fadeInLeft']} duration={1.25}>
+              <Text>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularis
+              </Text>
+            </AnimationBox>
+
+            <AnimationBox animateIn={['animate__fadeInUp']} duration={1.25}>
+              <Name>Ahmed Hridoy</Name>
+              <div>
+                <AiFillStar size={22} color="#F1B90B" />
+                <AiFillStar size={22} color="#F1B90B" />
+                <AiOutlineStar size={22} color="#F1B90B" />
+                <AiOutlineStar size={22} color="#F1B90B" />
+                <AiOutlineStar size={22} color="#F1B90B" />
+              </div>
+            </AnimationBox>
           </Info>
 
           <BoxForImg>
@@ -47,12 +55,16 @@ const Reviews = () => {
           </BoxForImg>
         </Box>
         <BoxForButtons>
-          <Btn>
-            <ArrowLeft size={32} />
-          </Btn>
-          <Btn>
-            <ArrowRight size={32} />
-          </Btn>
+          <AnimationBox animateIn={['animate__fadeInRight']} duration={1.25}>
+            <Btn>
+              <ArrowLeft size={32} />
+            </Btn>
+          </AnimationBox>
+          <AnimationBox animateIn={['animate__fadeInLeft']} duration={1.25}>
+            <Btn>
+              <ArrowRight size={32} />
+            </Btn>
+          </AnimationBox>
         </BoxForButtons>
       </ReviewsContainer>
     </ReviewsSection>
